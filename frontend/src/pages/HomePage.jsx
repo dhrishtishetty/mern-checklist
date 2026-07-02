e5e8114a0a33bc2d75a27cb230c85cdd699d20af
dhrishtishetty <dhrishtishetty10@gmail.com>
2026-07-02 16:15:55 +0530

    Change card title size and font weight

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -66,7 +66,7 @@ const HomePage = () => {
           <div className="card-body">
 
             <div className="flex justify-between items-center">
-              <h2 className="card-title text-2xl">
+              <h2 className="card-title text-xl sm:text-2xl font-semibold">
                 Today's Progress
               </h2>
 
